8df6c963afb39ea06a075e4f27c1cff7418a46cc
Hiddan13 <serega.mitkin13091@gmail.com>
2023-01-11 20:34:27 +0800

    hw02 v0.4

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,14 +9,10 @@ import (
 )
 
 const (
-	cMixed = "n0bb"
+	cMixed = "a4bc2d5e"
 	letter = "letter"
 	number = "number"
 )
-
-var itsSlash bool
-
-var itsDoubleslash bool
 
 var numberint int
 
@@ -24,62 +20,30 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(mixed string) (string, error) {
 	var result strings.Builder
-	var value, typ string
-	var value1, typ1 string
-	var runeline = []rune(mixed)
+	var value, typ, value1, typ1 string
+	runeline := []rune(mixed)
 	fmt.Println(runeline)
 	for _, c := range runeline {
 		typ, value = Define(c)
 		switch {
 		case typ == letter:
-			switch {
-			case typ1 == letter:
-				result.WriteString(value1)
-			case typ1 == "\\":
-				return "letter after \\", ErrInvalidString
-			}
-		case typ == "\\n" && value1 != "":
-			result.WriteString(value1)
-			value = "\\n"
-		case typ == "\\" && value1 != "" && !itsSlash:
-			itsSlash = true
 			if typ1 == letter {
 				result.WriteString(value1)
 			}
-		case typ == "\\" && typ1 == "\\" && !itsDoubleslash:
-			itsDoubleslash = true
-			itsSlash = false
 		case typ == number && value1 != "":
 			if s, err := strconv.Atoi(string(value)); err == nil {
 				numberint = s
 			}
 			switch {
-			case typ1 == number && !itsSlash && !itsDoubleslash:
+			case typ1 == number:
 				return "number after number", ErrInvalidString
-			case typ1 == letter || typ1 == "\\n":
+			case typ1 == letter:
 				if numberint == 0 {
 					break
 				}
 				result.WriteString(strings.Repeat(string(value1), numberint))
-			case itsDoubleslash:
-				if numberint == 0 {
-					break
-				}
-				result.WriteString(strings.Repeat(string(value1), numberint))
-				itsDoubleslash = false
-
-			case itsSlash:
-				if typ == number && typ1 == number {
-					if numberint == 0 {
-						break
-					}
-					result.WriteString(strings.Repeat(string(value1), numberint-1))
-					itsSlash = false
-					break
-				}
-				result.WriteString(value)
 			}
-		case typ == "\\n" && value1 == "" || typ == "\\" && value1 == "" || typ == number && value1 == "":
+		case typ == number && value1 == "":
 			return "first rune is not letter", ErrInvalidString
 		}
 		value1 = value
@@ -90,28 +54,17 @@ func Unpack(mixed string) (string, error) {
 	}
 	return result.String(), nil
 }
+
 func Define(c rune) (typ string, value string) {
-	switch {
-	case !unicode.IsDigit(c):
-		switch {
-		case c != 10 && c != 92:
-			res := letter
-			return res, string(c)
-		case c == 10: // \n
-			res := "\\n"
-			return res, string(c)
-		case c == 92: // \
-			res := "\\"
-			return res, string(c)
-		}
-	case unicode.IsDigit(c):
+	if unicode.IsDigit(c) {
 		res := number
 		return res, string(c)
+	} else {
+		res := letter
+		return res, string(c)
 	}
-	return
 }
 
 func main() {
 	fmt.Println(Unpack(cMixed))
-
 }
--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,72 @@
-package hw03frequencyanalysis
+package main
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+const text2 = ""
+
+var sSlice []string
+
+var valueSlice string
+
+var numWord = 0
+
+type Words struct {
+	Word string
+	Num  int
+}
+
+var mySlice []string
+
+func main() {
+	mySlice = Top10(text2)
+	fmt.Println(mySlice)
+}
+
+func Top10(t string) []string {
+	resultSlice := []Words{}
+	var res []string
+	sSlice = strings.Fields(t)
+	for _, x := range sSlice {
+		valueSlice = x
+		for _, c := range sSlice {
+			if valueSlice == c {
+				numWord++
+			}
+		}
+		a := Words{valueSlice, numWord}
+		resultSlice = append(resultSlice, a)
+		numWord = 0
+	}
+	sort.Slice(resultSlice, func(i, j int) bool {
+		// если одинаковое количество раз встречается - то сортируем лексеграфически
+		if resultSlice[i].Num == resultSlice[j].Num {
+			return resultSlice[i].Word < resultSlice[j].Word
+		}
+		return resultSlice[i].Num > resultSlice[j].Num // иначе просто по каличеству
+	})
+	ss := DelReplay(resultSlice)
+	for a, s := range ss {
+		if a == 10 {
+			break
+		} else {
+			res = append(res, s.Word)
+		}
+	}
+	return res
+}
+
+func DelReplay(typeSlice []Words) []Words {
+	keys := make(map[Words]bool)
+	list := []Words{}
+	for _, entry := range typeSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
 }
