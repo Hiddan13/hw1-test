fa636038cb8c539285f22704fcd378e699f15719
Hiddan13 <serega.mitkin13091@gmail.com>
2023-01-21 15:45:29 +0800

    hw03_test v0.4

--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -99,10 +99,14 @@ func Test_Top10my(t *testing.T) {
 			- Винни-Пух. Очень приятно!`,
 			expectedLetters: []string{"он", "бы", "и", "-", "Как", "с", "Винни-Пух.", "Другого", "Иногда", "Кристофером"},
 		},
-		{inputText: "aa a a b bbb b b e e ee",
-			expectedLetters: []string{"b", "a", "e", "a", "a", "bbb", "ee"}},
-		{inputText: "b k r k k s e rr e b b a e a a y yy ee ee a o po",
-			expectedLetters: []string{"a", "b", "e", "k", "ee", "o", "po", "r", "rr", "s"}},
+		{
+			inputText:       "aa a a b bbb b b e e ee",
+			expectedLetters: []string{"b", "a", "e", "a", "a", "bbb", "ee"},
+		},
+		{
+			inputText:       "b k r k k s e rr e b b a e a a y yy ee ee a o po",
+			expectedLetters: []string{"a", "b", "e", "k", "ee", "o", "po", "r", "rr", "s"},
+		},
 	}
 	for _, tC := range testCases {
 		tC := tC
