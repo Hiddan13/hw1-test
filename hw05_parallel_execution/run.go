62520307d1178e3023752d400e0fc3f8e3b4c18a
Hiddan13 <serega.mitkin13091@gmail.com>
2023-02-17 23:58:57 +0800

    hw05 v0.3

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -9,32 +9,10 @@ import (
 	"time"
 )
 
-// type Task struct {
-// 	V int
-// 	R bool
-// }
+var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
-var (
-	Err                    int
-	ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
-)
-
-// var m int
 type Task func() error
 
-//	var Slice = []Task{
-//		Task{300, false},
-//		Task{500, true},
-//		Task{200, false},
-//		Task{600, true},
-//		Task{1200, false},
-//		Task{6460, true},
-//		Task{2400, false},
-//		Task{2200, false},
-//		Task{6600, true},
-//		Task{7500, false},
-//		Task{6000, false},
-//	}
 var wg = sync.WaitGroup{}
 
 func main() {
@@ -42,66 +20,88 @@ func main() {
 	tasks := make([]Task, 0, tasksCount)
 
 	var runTasksCount int32
-	var sumTime time.Duration
 
 	for i := 0; i < tasksCount; i++ {
-
-		taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
-
-		sumTime += taskSleep
-
-		tasks = append(tasks, func() error { // Почему он не добавляет в слайс ? Вернее он добавляет, но что то не понятное
-
-			time.Sleep(taskSleep)
-
-			atomic.AddInt32(&runTasksCount, 1) // из за этого не заходит сюда и не возвращает false в error
-
-			return nil
+		err := fmt.Errorf("error from task %d", i)
+		tasks = append(tasks, func() error {
+			time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+			atomic.AddInt32(&runTasksCount, 1)
+			return err
 		})
 	}
 
-	workersCount := 5
-	maxErrorsCount := 1
-
-	// start := time.Now()
+	workersCount := 10
+	maxErrorsCount := 23
 	err := Run(tasks, workersCount, maxErrorsCount)
-	// err := Run(tasks, workersCount, maxErrorsCount)
 	if err != nil {
-		fmt.Println("error running tasks", Err, err)
+		fmt.Println("error running tasks", err)
 	}
-	defer fmt.Println(runTasksCount)
 
 	wg.Wait()
+
+	fmt.Println(runTasksCount, "<=", workersCount+maxErrorsCount)
 }
 
+// func main() {
+
+// 	tasksCount := 50
+// 	tasks := make([]Task, 0, tasksCount)
+
+// 	var runTasksCount int32
+// 	var sumTime time.Duration
+
+// 	for i := 0; i < tasksCount; i++ {
+// 		taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+// 		sumTime += taskSleep
+
+// 		tasks = append(tasks, func() error {
+// 			time.Sleep(taskSleep)
+// 			atomic.AddInt32(&runTasksCount, 1)
+// 			return nil
+// 		})
+// 	}
+
+// 	workersCount := 5
+// 	maxErrorsCount := 1
+
+// 	err := Run(tasks, workersCount, maxErrorsCount)
+
+// 	if err != nil {
+// 		fmt.Println("error running tasks", err)
+// 	}
+
+// 	wg.Wait()
+
+// 	fmt.Println("runTasksCount:", runTasksCount, "tasksCount", tasksCount)
+// }
+
 func Run(tasks []Task, n, m int) error {
-	ch := make(chan Task)
+	ch := make(chan error)
+	Err := 0
 
 	go func() {
 		for _, w := range tasks {
-			fmt.Println(&w)
-			if Err < m {
-				ch <- w
-				fmt.Println("write in ch")
-			} else {
-				fmt.Println("много ошибок")
-				break
+			if Err <= m {
+				ch <- w()
+
 			}
 		}
 		close(ch)
 	}()
-	for i := 0; i < n; i++ { // n
+	for i := 0; i < len(tasks); i++ { // n
 		wg.Add(1)
 
 		go func() {
 			for {
-				_, ok := <-ch
-				if !ok {
-					fmt.Println("err = ", ok)
+				t := <-ch
+				if t == nil {
 					break
-				} else {
-					fmt.Println("err =", ok)
+				} else if t != nil {
+
 					Err++
+
+					break
+
 				}
 
 			}
@@ -113,4 +113,5 @@ func Run(tasks []Task, n, m int) error {
 		return ErrErrorsLimitExceeded
 	}
 	return nil
+
 }
