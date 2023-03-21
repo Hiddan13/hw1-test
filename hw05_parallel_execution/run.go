a96f4fe215b90fcc4b862e0093581f40a1b6c061
Hiddan13 <serega.mitkin13091@gmail.com>
2023-03-21 17:30:49 +0700

    HW05 v0.5

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -15,90 +15,102 @@ type Task func() error
 
 var wg = sync.WaitGroup{}
 
-func main() {
-	tasksCount := 50
-	tasks := make([]Task, 0, tasksCount)
-
-	var runTasksCount int32
-
-	for i := 0; i < tasksCount; i++ {
-		err := fmt.Errorf("error from task %d", i)
-		tasks = append(tasks, func() error {
-			time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
-			atomic.AddInt32(&runTasksCount, 1)
-			return err
-		})
-	}
-
-	workersCount := 10
-	maxErrorsCount := 23
-	err := Run(tasks, workersCount, maxErrorsCount)
-	if err != nil {
-		fmt.Println("error running tasks", err)
-	}
-
-	wg.Wait()
-
-	fmt.Println(runTasksCount, "<=", workersCount+maxErrorsCount)
-}
-
 // func main() {
-
 // 	tasksCount := 50
 // 	tasks := make([]Task, 0, tasksCount)
 
 // 	var runTasksCount int32
-// 	var sumTime time.Duration
 
 // 	for i := 0; i < tasksCount; i++ {
-// 		taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
-// 		sumTime += taskSleep
-
+// 		err := fmt.Errorf("error from task %d", i)
 // 		tasks = append(tasks, func() error {
-// 			time.Sleep(taskSleep)
+// 			time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
 // 			atomic.AddInt32(&runTasksCount, 1)
-// 			return nil
+// 			return err
 // 		})
 // 	}
 
-// 	workersCount := 5
-// 	maxErrorsCount := 1
-
+// 	workersCount := 10
+// 	maxErrorsCount := 23
 // 	err := Run(tasks, workersCount, maxErrorsCount)
 // 	if err != nil {
 // 		fmt.Println("error running tasks", err)
 // 	}
+
 // 	wg.Wait()
-// 	fmt.Println("runTasksCount:", runTasksCount, "tasksCount", tasksCount)
+
+// 	fmt.Println(runTasksCount, "<=", workersCount+maxErrorsCount)
 // }
 
+func main() {
+
+	tasksCount := 50
+	tasks := make([]Task, 0, tasksCount)
+
+	var runTasksCount int32
+	var sumTime time.Duration
+
+	for i := 0; i < tasksCount; i++ {
+		taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+		sumTime += taskSleep
+
+		tasks = append(tasks, func() error {
+			time.Sleep(taskSleep)
+			atomic.AddInt32(&runTasksCount, 1)
+			return nil
+		})
+	}
+
+	workersCount := 5
+	maxErrorsCount := 1
+
+	err := Run(tasks, workersCount, maxErrorsCount)
+	if err != nil {
+		fmt.Println("error running tasks", err)
+	}
+	wg.Wait()
+	fmt.Println("runTasksCount:", runTasksCount, "tasksCount", tasksCount)
+}
+
 func Run(tasks []Task, n, m int) error {
-	ch := make(chan error)
-	Err := 0
-	go func() {
-		for _, w := range tasks {
-			if Err <= m {
-				ch <- w()
-			}
+	chTask := make(chan error)
+	ErrNum := 0
+	//fmt.Println("2 work")
+
+	for _, task := range tasks {
+		//fmt.Println("1 work")
+		//chTask := make(chan error)
+
+		if ErrNum < m {
+			//fmt.Println("56do work")
+
+			go func(t Task) {
+				fmt.Println("do work", t)
+				chTask <- t()
+
+			}(task)
+			//	close(chTask)
 		}
-		close(ch)
-	}()
+
+	}
+	//close(chTask)
+
 	for i := 0; i < len(tasks); i++ { // n
 		wg.Add(1)
 		go func() {
 			for {
-				t := <-ch
+				t := <-chTask
 				if t == nil {
 					break
 				} else if t != nil {
-					Err++
+					ErrNum++
 					break
 				}
 			}
 			wg.Done()
 		}()
 	}
-	if Err >= m {
+	if ErrNum >= m {
 		return ErrErrorsLimitExceeded
 	}
 	return nil
