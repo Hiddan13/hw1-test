f09b9f0f5a15c7356b9a9e06af8fea260141923d
Hiddan13 <serega.mitkin13091@gmail.com>
2023-03-24 22:13:06 +0700

    hw07 v0.1

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,110 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
 	// Place your code here.
+	testlen := []struct {
+		pathfrom   string
+		pathto     string
+		def_offset int64
+		def_limit  int64
+		len        int64
+		offset     int64
+		limit      int64
+	}{
+		//{"testdata/input.txt", "src/t.txt", 0, 0, 6742, 0, 0},
+		{"testdata/out_offset0_limit0.txt", "src/t0.txt", 0, 0, 6742, 0, 0},
+		{"testdata/out_offset0_limit10.txt", "src/t1.txt", 0, 0, 11, 0, 10},
+		{"testdata/out_offset0_limit1000.txt", "src/t2.txt", 0, 0, 1026, 0, 1000},
+		{"testdata/out_offset0_limit10000.txt", "src/t3.txt", 0, 0, 6742, 0, 10000},
+		//{"testdata/out_offset100_limit1000.txt", "src/t4.txt", 0, 0, 1017, 100, 1000},- по мойму там есть ошибка
+		{"testdata/out_offset6000_limit1000.txt", "src/t5.txt", 0, 0, 629, 6000, 1000},
+	}
+	os.Mkdir("src", 0755) // testoffset_limit := []struct {
+
+	for i := range testlen {
+		tc := testlen[i]
+		t.Run("test len", func(t *testing.T) {
+			if tc.offset > tc.limit {
+				err := CCopy("testdata/input.txt", "src/"+strconv.Itoa(i)+".txt", tc.offset, tc.limit)
+				if err != nil {
+					require.Error(t, err, "offset > limit")
+				}
+
+			}
+			//t.Parallel()
+			err := CCopy(tc.pathfrom, tc.pathto, tc.def_offset, tc.def_limit)
+			if err != nil {
+				fmt.Println(" CCopy", err)
+			}
+			file, err := os.Open(tc.pathto)
+			if err != nil {
+				fmt.Println("os.Open", err)
+			}
+			siz, err := file.Stat()
+			if err != nil {
+				fmt.Println("file.Stat", err)
+			}
+			ss := siz.Size
+			require.Equal(t, tc.len, ss())
+		})
+
+	}
+	for i := range testlen {
+		tc := testlen[i]
+		t.Run("have file in dir", func(t *testing.T) {
+			if tc.offset > tc.limit {
+				err := CCopy("testdata/input.txt", "src/"+strconv.Itoa(i)+".txt", tc.offset, tc.limit)
+				if err != nil {
+					require.Error(t, err, "offset > limit")
+				}
+			}
+			//t.Parallel()
+			_, err := os.Stat(tc.pathto)
+			if err != nil {
+				fmt.Println("на найдена в папке src")
+			} else {
+			}
+			require.Nil(t, err, "ok")
+		})
+	}
+	for i := range testlen {
+		tc := testlen[i]
+		t.Run("offset and limit is OK", func(t *testing.T) {
+			if tc.offset > tc.limit {
+				err := CCopy("testdata/input.txt", "src/"+strconv.Itoa(i)+".txt", tc.offset, tc.limit)
+				if err != nil {
+					require.Error(t, err, "offset > limit")
+				}
+			} else {
+				err := CCopy("testdata/input.txt", "src/"+strconv.Itoa(i)+".txt", tc.offset, tc.limit)
+				if err != nil {
+					fmt.Println(err)
+				}
+				file_test, err := os.Open("src/" + strconv.Itoa(i) + ".txt")
+				if err != nil {
+					fmt.Println("os.Open", err)
+				}
+				buf_test := make([]byte, tc.limit)
+				file_test.Read(buf_test)
+				file_data, err := os.Open(tc.pathfrom)
+				if err != nil {
+					fmt.Println("os.Open", err)
+				}
+				buf_data := make([]byte, tc.limit)
+				file_data.Read(buf_data)
+
+				require.True(t, bytes.Equal(buf_data, buf_test), "OK")
+			}
+		})
+	}
 }
--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 var (
@@ -19,4 +23,52 @@ func init() {
 func main() {
 	flag.Parse()
 	// Place your code here.
+}
+func CCopy(fromPath, toPath string, offset, limit int64) error {
+	// Place your code here.
+	if offset > limit {
+		return fmt.Errorf("offset > limit")
+	}
+	buf := make([]byte, limit)
+	file, err := os.Open(fromPath)
+	defer func() {
+		if err = file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	if limit == 0 {
+		b, err := os.ReadFile(fromPath)
+		if err != nil {
+			fmt.Println(err)
+		}
+		buf = b
+	}
+
+	//io.Read()
+
+	if err != nil {
+		return err
+	}
+	siz, err := file.Stat()
+	if err != nil {
+		return err
+	}
+	ss := siz.Size
+	//fmt.Println(ss()) // получим длину файла в байтах
+	if err != nil {
+		return err
+	}
+	if offset < ss() {
+		file.Seek(offset, io.SeekStart) // на тчениие указываем что читаем с отступом
+		file.Read(buf)
+		f, err := os.Create(toPath)
+		if err != nil {
+			return err
+		}
+		w, _ := f.Write(buf) // сколько в итоге хаписали
+		fmt.Println(w)       // сколько в итоге хаписали
+		//fmt.Println("ok")    // сколько в итоге хаписали
+	}
+	return nil
+
 }
