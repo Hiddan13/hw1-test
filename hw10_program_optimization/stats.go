5c982aaf2aa295e11165d772681dae15bfe3a16b
Hiddan13 <serega.mitkin13091@gmail.com>
2023-03-25 15:44:22 +0700

    hw10 v0.2

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,11 +1,11 @@
-package hw10programoptimization
+package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"regexp"
 	"strings"
 )
 
@@ -19,6 +19,19 @@ type User struct {
 	Address  string
 }
 
+func main() {
+	data := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
+{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+	result, err := GetDomainStat(bytes.NewBufferString(data), "com")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(result)
+}
+
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
@@ -29,7 +42,7 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 	return countDomains(u, domain)
 }
 
-type users [100_000]User
+type users [10]User
 
 func getUsers(r io.Reader) (result users, err error) {
 	content, err := ioutil.ReadAll(r)
@@ -45,6 +58,7 @@ func getUsers(r io.Reader) (result users, err error) {
 				return
 			}
 			result[ii] = user
+			fmt.Println(user.Email)
 		}(i, line)
 	}
 	return
@@ -54,16 +68,22 @@ func countDomains(u users, domain string) (DomainStat, error) {
 	result := make(DomainStat)
 
 	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
-		if err != nil {
-			return nil, err
-		}
+		//matched, err := regexp.Match("."+domain, []byte(user.Email))
+		matched := strings.Contains("."+domain, user.Email)
+		fmt.Println(domain, user.Email)
+		fmt.Println(len(u))
 
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
-		}
+		fmt.Println(matched)
+		// if err != nil {
+		// 	return nil, err
+		// }
+
+		// if matched {
+		// 	//num [i] = user
+		// 	num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
+		// 	num++
+		// 	result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+		// }
 	}
 	return result, nil
 }
