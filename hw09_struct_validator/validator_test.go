d2a351be21976cbad6a144894f0f6b95b612efe5
Hiddan13 <serega.mitkin13091@gmail.com>
2023-04-19 23:14:01 +0700

    hw09_v0.1

--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -1,9 +1,11 @@
-package hw09structvalidator
+package main
 
 import (
 	"encoding/json"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -34,27 +36,52 @@ type (
 		Code int    `validate:"in:200,404,500"`
 		Body string `json:"omitempty"`
 	}
+
+	Check struct {
+		Ms []string `validate:"len:1"`
+		Mi []int    `validate:"max:1"`
+	}
 )
 
 func TestValidate(t *testing.T) {
+	var ve ValidationErrors
 	tests := []struct {
 		in          interface{}
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			in:          &User{ID: "333", Name: "Bil", Age: 8, meta: nil},
+			expectedErr: ValidationErrors{ValidationError{Field: "Age", Err: ErrMin}},
 		},
-		// ...
-		// Place your code here.
+		{
+			in:          &User{ID: "0", Name: "Bod", Age: 44},
+			expectedErr: ve,
+		},
+		{
+			in:          &Check{Mi: []int{1, 22}},
+			expectedErr: ValidationErrors{ValidationError{Field: "Mi", Err: ErrMax}},
+		},
+		{
+			in:          &[]int{1, 2},
+			expectedErr: ErrStruct,
+		},
+		{
+			in:          &User{ID: "1234566", Email: "ii@gmail"},
+			expectedErr: ValidationErrors{ValidationError{Field: "Email", Err: ErrRegExp}},
+		},
+		{
+			in:          &App{Version: "1.2.2.222"},
+			expectedErr: ValidationErrors{ValidationError{Field: "Version", Err: ErrLen}},
+		},
 	}
 
 	for i, tt := range tests {
 		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
 			tt := tt
 			t.Parallel()
-
-			// Place your code here.
-			_ = tt
+			if err := Validate(tt.in); err != nil {
+				require.Equal(t, tt.expectedErr, err)
+			}
 		})
 	}
 }
