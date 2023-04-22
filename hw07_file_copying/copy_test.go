31aac8cdcbb7c8adb95aafba292a9df468b47187
Hiddan13 <serega.mitkin13091@gmail.com>
2023-04-22 19:37:56 +0700

    hw07_v0.8

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -21,17 +21,16 @@ func TestCopy(t *testing.T) {
 		offset   int64
 		limit    int64
 	}{
-		{"testdata/input.txt", "testdata/out_offset0_limit0.txt", "src1/t0.txt", 0, 0, 6742, 0, 0},
-		{"testdata/input.txt", "testdata/out_offset0_limit10.txt", "src1/t1.txt", 0, 0, 11, 0, 10},
-		{"testdata/input.txt", "testdata/out_offset0_limit1000.txt", "src1/t2.txt", 0, 0, 1026, 0, 1000},
-		{"testdata/input.txt", "testdata/out_offset0_limit10000.txt", "src1/t3.txt", 0, 0, 6742, 0, 10000},
-		{"testdata/input.txt", "testdata/out_offset6000_limit1000.txt", "src1/t5.txt", 0, 0, 629, 6000, 1000},
+		{"testdata/input.txt", "testdata/out_offset0_limit0.txt", "t0.txt", 0, 0, 6742, 0, 0},
+		{"testdata/input.txt", "testdata/out_offset0_limit10.txt", "t1.txt", 0, 0, 11, 0, 10},
+		{"testdata/input.txt", "testdata/out_offset0_limit1000.txt", "t2.txt", 0, 0, 1026, 0, 1000},
+		{"testdata/input.txt", "testdata/out_offset0_limit10000.txt", "t3.txt", 0, 0, 6742, 0, 10000},
+		{"testdata/input.txt", "testdata/out_offset6000_limit1000.txt", "t5.txt", 0, 0, 629, 6000, 1000},
 	}
-	os.Mkdir("src1", 0o755)
 	for i := range testlen {
 		tc := testlen[i]
 		t.Run("test len", func(t *testing.T) {
-			err := Copy(tc.idelFile, "src1/"+strconv.Itoa(i)+".txt", tc.offset, tc.limit)
+			err := Copy(tc.idelFile, strconv.Itoa(i)+".txt", tc.offset, tc.limit)
 			if err != nil {
 				require.Error(t, err, "offset > limit")
 			} else {
@@ -52,11 +51,11 @@ func TestCopy(t *testing.T) {
 			}
 		})
 		t.Run("offset and limit is OK", func(t *testing.T) {
-			err := Copy(tc.idelFile, "src1/"+strconv.Itoa(i)+".txt", tc.offset, tc.limit)
+			err := Copy(tc.idelFile, strconv.Itoa(i)+".txt", tc.offset, tc.limit)
 			if err != nil {
 				require.Error(t, err, "offset > limit")
 			} else {
-				fileTest, err := os.Open("src1/" + strconv.Itoa(i) + ".txt")
+				fileTest, err := os.Open(strconv.Itoa(i) + ".txt")
 				require.NoError(t, err)
 				bufTest := make([]byte, tc.limit)
 				fileTest.Read(bufTest)
