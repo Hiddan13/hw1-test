f938d44ee40ccb342a6c6deef4d07c7ec23ca319
Hiddan13 <serega.mitkin13091@gmail.com>
2023-04-22 19:51:39 +0700

    hw07_v0.11

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"log"
 	"os"
 	"strconv"
 	"testing"
@@ -15,41 +14,18 @@ func TestCopy(t *testing.T) {
 		idelFile string
 		pathfrom string
 		pathto   string
-		offset0  int64
-		limit0   int64
 		len      int64
 		offset   int64
 		limit    int64
 	}{
-		{"testdata/input.txt", "testdata/out_offset0_limit0.txt", "t0.txt", 0, 0, 6742, 0, 0},
-		{"testdata/input.txt", "testdata/out_offset0_limit10.txt", "t1.txt", 0, 0, 11, 0, 10},
-		{"testdata/input.txt", "testdata/out_offset0_limit1000.txt", "t2.txt", 0, 0, 1026, 0, 1000},
-		{"testdata/input.txt", "testdata/out_offset0_limit10000.txt", "t3.txt", 0, 0, 6742, 0, 10000},
-		{"testdata/input.txt", "testdata/out_offset6000_limit1000.txt", "t5.txt", 0, 0, 629, 6000, 1000},
+		{"testdata/input.txt", "testdata/out_offset0_limit0.txt", "t0.txt", 6742, 0, 0},
+		{"testdata/input.txt", "testdata/out_offset0_limit10.txt", "t1.txt", 11, 0, 10},
+		{"testdata/input.txt", "testdata/out_offset0_limit1000.txt", "t2.txt", 1026, 0, 1000},
+		{"testdata/input.txt", "testdata/out_offset0_limit10000.txt", "t3.txt", 6742, 0, 10000},
+		{"testdata/input.txt", "testdata/out_offset6000_limit1000.txt", "t5.txt", 629, 6000, 1000},
 	}
 	for i := range testlen {
 		tc := testlen[i]
-		t.Run("test len", func(t *testing.T) {
-			err := Copy(tc.idelFile, strconv.Itoa(i)+".txt", tc.offset, tc.limit)
-			if err != nil {
-				require.Error(t, err, "offset > limit")
-			} else {
-				if err := Copy(tc.pathfrom, tc.pathto, tc.offset0, tc.limit); err != nil {
-					log.Fatal(err)
-				}
-				file, err := os.Open(tc.pathto)
-				require.NoError(t, err)
-				siz, err := file.Stat()
-				require.NoError(t, err)
-				require.Nil(t, err, "ok")
-				ss := siz.Size
-				if tc.limit != 0 {
-					require.Equal(t, tc.limit, ss())
-				} else {
-					require.Equal(t, tc.len, ss())
-				}
-			}
-		})
 		t.Run("offset and limit is OK", func(t *testing.T) {
 			err := Copy(tc.idelFile, strconv.Itoa(i)+".txt", tc.offset, tc.limit)
 			if err != nil {
